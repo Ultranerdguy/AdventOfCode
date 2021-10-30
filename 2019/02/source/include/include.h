abda8ae83024d485151f2eb9acb7e35fdb7d548b
Ultranerdguy <noreply@localhost>
2021-10-30 14:37:14 +0100

    2019-03 Built the basis

--- 2019/02/source/include/include.h
+++ 2019/02/source/include/include.h
@@ -1,6 +1,8 @@
 #ifndef _AOC_INCLUDE_H_
 #define _AOC_INCLUDE_H_
 
+#include <cstdlib>
+
 using OPCODE = int;
 enum Op {ADD = 1, MUL = 2, END = 99};
 
---
+++ 2019/03/source/include/include.h
@@ -0,0 +1,79 @@
+#ifndef _AOC_INCLUDE_H_
+#define _AOC_INCLUDE_H_
+
+#include <cmath>
+#include <vector>
+
+class Point
+{
+public:
+  inline constexpr Point(const int _x = 0, const int _y = 0);
+  inline int Length2();
+  int x;
+  int y;
+};
+
+inline constexpr int Dot(const Point& lhs, const Point& rhs);
+inline constexpr Point operator+(const Point& lhs, const Point& rhs);
+inline constexpr Point operator-(const Point& lhs, const Point& rhs);
+inline constexpr Point operator-(const Point& pnt);
+inline constexpr Point operator*(const int lhs, const Point& rhs);
+inline constexpr Point operator*(const Point& lhs, const int rhs);
+
+class WireManager
+{
+public:
+  void AddWirePoint(std::size_t wire, const Point& point);
+
+  std::vector<Point> GetIntersections(std::size_t wire1, std::size_t wire2);
+  std::vector<Point> GetIntersections(const Point& A1, const Point& A2, const Point& B1, const Point& B2);
+  Point GetClosestPoint(const std::vector<Point>& points);
+
+protected:
+  std::vector<std::vector<Point>> m_wires;
+};
+
+// Implementation
+// For templates and constexpr only
+
+constexpr Point::Point(const int _x, const int _y)
+  : x(_x)
+  , y(_y)
+{}
+
+int Point::Length2()
+{
+  return x*x + y*y;
+}
+
+constexpr int Dot(const Point& lhs, const Point& rhs)
+{
+  return lhs.x*rhs.x + lhs.y*rhs.y;
+}
+
+constexpr Point operator+(const Point& lhs, const Point& rhs)
+{
+  return Point(lhs.x + rhs.x, lhs.y + rhs.y);
+}
+
+constexpr Point operator-(const Point& lhs, const Point& rhs)
+{
+  return Point(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+constexpr Point operator-(const Point& pnt)
+{
+  return Point(-pnt.x, -pnt.y);
+}
+
+constexpr Point operator*(const int lhs, const Point& rhs)
+{
+  return Point(rhs.x * lhs, rhs.y * lhs);
+}
+
+constexpr Point operator*(const Point& lhs, const int rhs)
+{
+  return rhs * lhs;
+}
+
+#endif
