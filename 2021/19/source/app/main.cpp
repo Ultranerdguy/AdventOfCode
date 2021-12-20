3f9a3ed3d42a3f30479e3f586faa1aae0bb76d43
Sam Clifton <samclifton1729@gmail.com>
2021-12-20 13:27:18 +0000

    2021_19 Began day 19

---
+++ 2021/19/source/app/main.cpp
@@ -0,0 +1,21 @@
+// C++ include files
+#include <iostream>
+#include <fstream>
+
+// Project specific files
+#include "include.h"
+#include "config.h"
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2)
+  {
+    std::cout << "Usage: \n";
+    std::cout << "  " << EXECUTABLE << " <input file>\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  
+  
+  return 0;
+}
